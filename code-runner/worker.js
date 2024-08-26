a14a31b360f3ba590828fab7807e6f6c3262d48a
Piyush Kumar <mr.piyush.cv@gmail.com>
2024-08-26 11:11:29 +0530

    worker refactor

--- code-runner/worker.js
+++ code-runner/worker.js
@@ -3,48 +3,88 @@ import { compile } from "./compile.js";
 import { run } from "./run.js";
 import fs from "fs";
 
-const client = createClient();         
+const client = createClient();
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
 client.connect();
 
 client.on("ready", () => {
-    console.log("Redis Client Ready");
+  console.log("Redis Client Ready");
 
-    processQueue();
-    
+  processQueue();
 });
 
-
 const processQueue = async () => {
-    while (true) {
-        const submission = await client.rPop("submissions");
-        if (submission) {
-            const submissionJson = JSON.parse(submission);
-            client.publish(`${submissionJson.studentId}-${submissionJson.questionId}`, JSON.stringify({ "start" : true }));
-            //compile 
-            const compileResult  = await compile(submissionJson.solutionFilePath , submissionJson.dirPath);
+  while (true) {
+    const submission = await client.rPop("submissions");
+    if (submission) {
+      let submissionJson;
+      try {
+        submissionJson = JSON.parse(submission);
+      } catch (error) {
+        console.log("Error parsing submission", submission);
+        continue;
+      }
 
-            client.publish(`${submissionJson.studentId}-${submissionJson.questionId}`, JSON.stringify({ ...compileResult  }));
-            // run
-            console.log("testcases", submissionJson.testCases);
-            
-            const result = await run(submissionJson.solutionFilePath , submissionJson.dirPath , submissionJson.testCases , client); 
+      try {
+        client.publish(
+          `${submissionJson.studentId}-${submissionJson.questionId}`,
+          JSON.stringify({ start: true })
+        );
+        //compile
+        const compileResult = await compile(
+          submissionJson.solutionFilePath,
+          submissionJson.dirPath
+        );
 
-            // save the output json in the submissionJson.dirPath
-            const outputPath = `${submissionJson.dirPath}/output.json`;
-            const outputJson = { ...result , studentId : submissionJson.studentId , questionId : submissionJson.questionId , "end" : true };
-            fs.writeFileSync(outputPath, JSON.stringify(outputJson));
+        client.publish(
+          `${submissionJson.studentId}-${submissionJson.questionId}`,
+          JSON.stringify({ ...compileResult })
+        );
+        // run
+        // console.log("testcases", submissionJson.testCases);
 
+        const result = await run(
+          submissionJson.solutionFilePath,
+          submissionJson.dirPath,
+          submissionJson.testCases,
+          client
+        );
 
-            client.publish(`${submissionJson.studentId}-${submissionJson.questionId}`, JSON.stringify(outputJson));
+        // save the output json in the submissionJson.dirPath
+        const outputPath = `${submissionJson.dirPath}/output.json`;
+        const outputJson = {
+          ...result,
+          studentId: submissionJson.studentId,
+          questionId: submissionJson.questionId,
+          end: true,
+        };
+        fs.writeFileSync(outputPath, JSON.stringify(outputJson));
 
-            console.log(`Running submission: ${submissionJson.studentId} ${submissionJson.questionId}`, result);
-        }
-        else {
-            // wait a sec 
-            await new Promise(resolve => setTimeout(resolve, 1000));
-        }
+        client.publish(
+          `${submissionJson.studentId}-${submissionJson.questionId}`,
+          JSON.stringify(outputJson)
+        );
+
+        console.log(
+          `Running submission: ${submissionJson.studentId} ${submissionJson.questionId}`,
+          result
+        );
+      } catch (error) {
+        client.publish(
+          `${submissionJson.studentId}-${submissionJson.questionId}`,
+          JSON.stringify({
+            ...error,
+            studentId: submissionJson.studentId,
+            questionId: submissionJson.questionId,
+            end: true,
+          })
+        );
+      }
+    } else {
+      // wait a sec
+      await new Promise((resolve) => setTimeout(resolve, 1000));
     }
+  }
 };
